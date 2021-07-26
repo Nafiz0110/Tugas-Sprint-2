a7c7b3410c22004a219a5177d6ac107e6a4cb44a
@Nafizzz <87061342+Nafiz0110@users.noreply.github.com>
2021-07-26 17:21:55 +0700

    Delete Sprint2 - T1 (Nomer1) directory

--- Sprint2 - T1 (Nomer1)/luaspersegipanjang.js
+++
@@ -1,9 +0,0 @@
-function hitungLuasPersegiPanjang(panjang,lebar) {
-        luas = panjang * lebar
-        document.write(`Panjang : ` + ipanjang + "<br/>")
-        document.write(`Lebar : ` + ilebar + "<br/>")
-        document.write(`Luas Persegi Panjang : ` + luas + "<br/>")
-}
-ipanjang = prompt("Masukan Nilai Panjang");
-ilebar= prompt("Masukan NIlai Lebar");
-hitungLuasPersegiPanjang(panjang,lebar)
