4156464f409a0f50fe5a5e7a5f5f2f1d200bf94c
@Nafizzz <87061342+Nafiz0110@users.noreply.github.com>
2021-07-26 17:01:36 +0700

    Tugas Sprint2 - Nomer 1

---
+++ Sprint2 - T1 (Nomer1)/sp2-tugas1.js
@@ -0,0 +1,7 @@
+function hitungLuasPersegiPanjang(panjang,lebar) {
+        luas = panjang * lebar
+        document.write(`Luas Persegi Panjang : ` + luas)
+}
+panjang = prompt("Masukan Nilai Panjang");
+lebar= prompt("Masukan NIlai Lebar");
+hitungLuasPersegiPanjang(panjang,lebar)
