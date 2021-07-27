2021ec5a0fbbb9602632940ecea2d1e067d9e19c
@Nafizzz <87061342+Nafiz0110@users.noreply.github.com>
2021-07-27 11:40:12 +0700

    Sprint2 -Tugas2 (Array)

---
+++ Sprint2 - T2 (Nomer1) Array/Listdesa.js
@@ -0,0 +1,6 @@
+let listDesaBantul  = [
+['Danurejan : ', 'Bausasran','Tegal Panggung','Suryatmajan'],
+['Gedong Tengen :','Sosromenduran','Pringgokusuman'],
+['Gondomanann : ','Prawirodirjan','Ngupasan']
+];
+console.log(listDesaBantul);
