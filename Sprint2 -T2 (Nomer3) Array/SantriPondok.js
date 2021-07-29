70a7741bf68bdd4de27c7a11168ae64fad0dcd06
@Nafizzz <87061342+Nafiz0110@users.noreply.github.com>
2021-07-29 09:48:37 +0700

    Sprint2 - Tugas 2 (Array)

--- Sprint2 -T2 (Nomer3) Array/SantriPondok.js
+++ Sprint2 -T2 (Nomer3) Array/SantriPondok.js
@@ -1,4 +1,4 @@
 let santriPondok = ['Wandi','Aldi','Acep','Putra','Hafif','Syofyan','Rahmad'];
-for(let i = 1; i < santriPondok.length; i++) {
-        console.log(i + '.' + santriPondok[i]);
+for(let i = 0; i < santriPondok.length; i++) {
+        console.log(i + 1 +  '.' + santriPondok[i]);
     }
